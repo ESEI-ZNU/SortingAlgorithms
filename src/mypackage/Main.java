1c1aea7abb649cb03556565dbee928aaec4e2fc6
Hennadii Kolomoiets <kgp@znu.edu.ua>
2025-02-07 16:19:47 +0200

    Доданий код, отримання від користувача розміру масиву.

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,7 +1,13 @@
 package mypackage;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Якого розміру масив Ви хочете створити?");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+        int arraySize = Integer.parseInt(answer);
+        System.out.println("Ви обрали масив з " + arraySize + " елементами цілого типу.");
     }
 }

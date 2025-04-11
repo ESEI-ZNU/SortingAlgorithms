b7babbf97beecf9350df2fa59a0847a0c70a7cca
dudova-lisa <lizadudova91@gmail.com>
2025-04-11 16:00:16 +0200

    add: class SortTester add sorting methods: Bubble Sort and Insertion Sort

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -39,5 +39,73 @@ public class Main {
 
         System.out.println("Ваш масив:");
         System.out.println(Arrays.toString(myArray.getArr()));
+        System.out.println("А який алгоритм ви хочете обрати?");
+
+        System.out.println("1 — Bubble Sort");
+
+        System.out.println("2 — Insertion Sort");
+
+        int sortOption = scanner.nextInt();
+        SortTester tester = new SortTester(myArray.getArr());
+        if (sortOption == 1) {
+            tester.runBubbleSort();
+        } else if (sortOption == 2) {
+            tester.runInsertionSort();
+        } else {
+            System.out.println("Невірний вибір. Сортування не буде виконано.");
+        }
     }
 }
+
+class SortTester {
+    private int[] array;
+    private long comparisons;
+    private long swaps;
+    public SortTester(int[] array) {
+        this.array = array;
+    }
+    public void runBubbleSort() {
+        comparisons = 0;
+        swaps = 0;
+        long startTime = System.nanoTime();
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                comparisons++;
+                if (array[j] > array[j + 1]) {
+                    swaps++;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Bubble Sort Time: " + (endTime - startTime) + " ns");
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Swaps: " + swaps);
+    }
+    public void runInsertionSort() {
+        comparisons = 0;
+        swaps = 0;
+        long startTime = System.nanoTime();
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                comparisons++;
+                array[j + 1] = array[j];
+                swaps++;
+                j--;
+            }
+            array[j + 1] = key;
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Insertion Sort Time: " + (endTime - startTime) + " ns");
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Swaps: " + swaps);
+
+    }
+
+}
+
+
